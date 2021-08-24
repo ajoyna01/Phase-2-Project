383e848aa5d051b5ddd4039670c66b611266643a
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-08-24 09:50:40 -0500

    Initial Commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,53 @@
 import React from "react";
 
-function PlantCard(){
+function PlantCard(plants){
+  console.log("Plant Card", plants)
 
 
 
 
-  return(
 
-  )
+  return(<>  - Plant Card -    
+  <div className="main plant card div">
+  <div
+    className="plant card"
+    key={plants.id}
+    onClick={()=> 
+      console.log("add event listener")
+    }>
+    <div className="image">
+      <img alt="don't kill me" src={plants.image_url} />
+    </div>
+    <div className="plants specs">
+      <div className=" plant name">
+        {plants.name}
+        
+      </div>
+      <div className="plant water">  
+        <h4> {plants.water}</h4>
+       </div> 
+      <div className="plant light">  
+        <h4> {plants.light}</h4>
+       </div> 
+      <div className="plant description">
+        <h3> {plants.description}</h3>
+      </div>  
+    </div>
+          <button
+            className="favorites"
+            onClick={() => 
+              console.log("add event listener")
+            }
+          >
+            x
+          </button>
+        
+  </div>
+</div>
+);
+
+
+  </>)
 }
 
 export default PlantCard;
--- src/components/PlantsPage.js
+++ src/components/PlantsPage.js
@@ -1,19 +1,22 @@
-import { useEffect, useState } from 'react'
+import React , { useEffect, useState } from 'react'
+import PlantCard from './PlantCard';
 
 
 
 
 const PlantsPage =()=>{
       console.log("In PlantsPage")
+    //   console.log(json)
 
 
     const [plants, setPlants] = useState([])
         console.log("State of our plants[ ", plants, " ]")
+        
 
 
         const fetchPlants=()=>{ console.log("In useEffect") 
 
-            fetch('http://www.quantitative-plant.org/api?type=TYPE&field=FIELD&q=Q')
+            fetch('http://localhost:3000/plants')
             .then(response => response.json())
             .then(data => { console.log("From Our Fetch:  ", data) 
             
@@ -33,7 +36,7 @@ const PlantsPage =()=>{
         {
             plants.map(eachPlant =>{ console.log(eachPlant) 
             
-                return(<h4>{eachPlant.name}</h4>)
+                return(<h4> <PlantCard key={eachPlant.name}/> </h4>)
 
             })
         }
@@ -43,4 +46,4 @@ const PlantsPage =()=>{
 
 
 }
-export default PlantsPage
+export default PlantsPage;
